ad54c71a5a5be774ddc83574262456e50f7c07f2
David Levy <davidl@codefresh.io>
2017-05-03 14:16:19 +0300

    define username & password from env vars.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
 let tty = require('tty.js');
 
+let users = {};
+
+if(process.env.USERNAME) {
+  users[process.env.USERNAME] = process.env.PASSWORD;
+} else {
+  users.user = 'pass';
+}
+
 let app = tty.createServer({
   shell: 'bash',
-  users: {
-    foo: 'bar'
-  },
+  users: users,
   cwd: process.env.FOLDER || '.',
   port: 8000
 });
